a9d68f9c8057f19e9321fecf91195d5a0361615a
Marcus Jaiclin <marcus.jaiclin@bishops.com>
2024-09-20 20:26:20 -0700

    Fix transfer.py to drop the final layer. Add prefetch.

--- transfer.py
+++ transfer.py
@@ -8,9 +8,10 @@ import tensorflow.keras.layers as layers
 import tensorflow.keras.losses as losses
 import tensorflow.keras.optimizers as optimizers
 import tensorflow.keras.utils as utils
+import tensorflow.data as data
 from tensorflow.data import Dataset
 
-BATCH_SIZE = 16
+BATCH_SIZE = 32
 
 train, validation = utils.image_dataset_from_directory(
     'defungi',
@@ -22,23 +23,26 @@ train, validation = utils.image_dataset_from_directory(
     image_size = (224, 224),
     shuffle = True,
     seed = 8008,
-    validation_split = 0.15,
+    validation_split = 0.2,
     subset = 'both',
 )
 
 # Prepare train and test sets for input into ResNet
 train = train.map(lambda x, y: (resnet50.preprocess_input(x), y))
 validation = validation.map(lambda x, y: (resnet50.preprocess_input(x), y))
+train = train.cache().prefetch(buffer_size = data.AUTOTUNE)
+validation = validation.cache().prefetch(buffer_size = data.AUTOTUNE)
 
 # Load the ResNet architecture
 res = resnet50.ResNet50(
-    # Need the classification layer to flatten
-    include_top = True,
-    classifier_activation = None,
-    # Load trained weights, not just the architecture
-    weights = 'imagenet',
-    # Gotta use this size if include_top = True
-    input_shape = (224, 224, 3),
+        # Remove the final fully-connected/linear layer by setting include_top
+        #   to False
+        include_top = False,
+        classifier_activation = None,
+        # Load trained weights, not just the architecture
+        weights = 'imagenet',
+        # This is the expected input size for Resnet
+        input_shape = (224, 224, 3),
     )
 
 # Freeze the ResNet weights so we only train the transfer layer
@@ -52,11 +56,13 @@ inputs = tf.keras.Input(shape = (224, 224, 3))
 
 # First, send image through ResNet:
 outputs = res(inputs)
+# Flatten so we can send through the Dense layer
+outputs = layers.Flatten()(outputs)
 # Then send it to a classification layer
 outputs = layers.Dense(5, activation = 'softmax')(outputs)
 
 # Set optimizer and loss
-optimizer = optimizers.legacy.Adam(learning_rate = .00001)
+optimizer = optimizers.legacy.Adam(learning_rate = .0001)
 loss = losses.CategoricalCrossentropy()
 
 # Now, define the model, using above inputs and outputs
@@ -71,8 +77,7 @@ model.compile(
 model.fit(
     train,
     batch_size = BATCH_SIZE,
-    epochs = 250,
+    epochs = 3,
     verbose = 1,
     validation_data = validation,
-    validation_batch_size = 32,
 )
